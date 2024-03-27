f0628ec68a58e1907fcacd46633bec0f86250617
Midorichie <hamsohood@gmail.com>
2024-03-27 14:18:18 +0100

    Left

--- 1-binary_tree_insert_left.c
+++ 1-binary_tree_insert_left.c
@@ -24,7 +24,7 @@ binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
 	newnode->left = NULL;
 	newnode->right = NULL;
 
-	if (parent->left !=NULL)
+	if (parent->left != NULL)
 	{
 		newnode->left = parent->left;
 		parent->left = newnode;
